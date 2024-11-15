e6cbc616fb6383655fcbab4c9bed7d330fc7a7ff
Kojer <134270504+QuocKo@users.noreply.github.com>
2024-11-15 17:31:13 +0700

    fix DonationGetByUserID
    fix DonationGetByUserID

---
+++ ChildrenVillageSOS_DAL/DTO/DonationDTO/DonationResponseDTO.cs
@@ -0,0 +1,33 @@
+﻿using ChildrenVillageSOS_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildrenVillageSOS_DAL.DTO.DonationDTO
+{
+    public class DonationResponseDTO
+    {
+        public int Id { get; set; }
+
+        public string UserAccountId { get; set; }
+
+        public string DonationType { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public virtual UserAccount UserAccount { get; set; }
+    }
+}
