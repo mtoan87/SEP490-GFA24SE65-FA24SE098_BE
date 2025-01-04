be1920ccbafb34dcf6d352fb43eae0d7db373690
Jockey780 <xlordhitmanx@gmail.com>
2025-01-04 22:54:28 +0700

    Update the TransferRequest

---
+++ ChildrenVillageSOS_DAL/Enum/TransferStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildrenVillageSOS_DAL.Enum
+{
+    public enum TransferStatus
+    {
+        Pending,
+        Approved,
+        Completed,
+        Rejected
+    }
+}
